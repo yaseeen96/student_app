f76cd2fa080f829fb13c2fe98178c460606d23e4
ven-p1 <venkateshpatil193@gmail.com>
2022-09-09 09:37:29 +0530

    Add user home and settings page

--- lib/views/user/user_page.dart
+++ lib/views/user/user_page.dart
@@ -3,93 +3,216 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:student_app/models/student_login_model.dart';
 import 'package:student_app/views/login/login_page.dart';
-// import 'package:flutter/src/widgets/container.dart';
-// import 'package:flutter/src/widgets/framework.dart';
 
 //user-defined imports
 import '../../utils/configurations.dart';
+import 'user_settings_page.dart';
+import '../event/event_detail.dart';
 
-class UserPage extends StatelessWidget {
+class UserPage extends StatefulWidget {
   const UserPage({
     super.key,
   });
 
-  // final StudentLoginModel _user;
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> with TickerProviderStateMixin {
+  late TabController _tabBarContoller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _tabBarContoller = TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    double unitHeightValue = MediaQuery.of(context).size.height * 0.01;
     final deviceWidth = getDeviceWidth(context);
     final deviceHeight = getDeviceHeight(context);
     return Scaffold(
       backgroundColor: Theme.of(context).highlightColor,
       appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        // elevation: 0,
         toolbarHeight: deviceHeight * 0.10,
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: Theme.of(context).highlightColor,
         centerTitle: false,
         title: Text(
           "Welcome, Yaseen",
           style: TextStyle(
-              color: Theme.of(context).highlightColor,
-              fontSize: 20,
+              color: Theme.of(context).primaryColor,
+              fontSize: 2.5 * unitHeightValue,
               fontWeight: FontWeight.bold),
         ),
         actions: [
           IconButton(
             padding: EdgeInsets.all(deviceWidth * 0.05),
-            iconSize: deviceHeight * 0.05,
-            onPressed: () async {
-              SharedPreferences prefs = await SharedPreferences.getInstance();
-              prefs.remove("token");
-
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LoginPage(),
-                ),
-              );
-            },
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => UserSettingsPage())),
             icon: Icon(
-              Icons.logout,
+              Icons.account_circle,
+              // size: 40,
             ),
-          ),
-          // IconButton(
-          //     padding: EdgeInsets.all(deviceWidth * 0.05),
-          //     iconSize: deviceHeight * 0.05,
-          //     onPressed: () {},
-          //     icon: Icon(
-          //       Icons.account_profile,
-          //     ))
+            iconSize: deviceHeight * 0.06,
+            color: Colors.black,
+          )
         ],
       ),
       body: Container(
-        padding: EdgeInsets.all(deviceWidth * 0.03),
-        constraints: BoxConstraints(maxHeight: deviceHeight * 0.22),
-        decoration: BoxDecoration(border: Border.all()),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
+        height: deviceHeight,
+        child: ListView(
           children: [
-            // picture
-            Container(
-              margin: EdgeInsets.only(right: deviceWidth * 0.05),
-              width: deviceWidth * 0.38,
-              height: deviceHeight * 0.2,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/blockchain.png"),
-                ),
+            TabBar(
+              unselectedLabelColor: Colors.grey,
+              labelColor: Theme.of(context).primaryColor,
+              controller: _tabBarContoller,
+              tabs: [
+                Tab(text: "Upcoming"),
+                Tab(text: "Completed"),
+              ],
+            ),
+            // TabBarView(
+            //   controller: _tabBarContoller,
+            //   children: [
+            //     Upcoming(),
+            //     Completed(),
+            //   ],
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Upcoming extends StatelessWidget {
+  const Upcoming({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double unitHeightValue = MediaQuery.of(context).size.height * 0.01;
+    final deviceWidth = getDeviceWidth(context);
+    final deviceHeight = getDeviceHeight(context);
+    return Container(
+      // height: getDeviceHeight(context) * .4,
+      child: ListView(
+        children: [
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+          SizedBox(
+            height: deviceHeight * 0.02,
+          ),
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+          SizedBox(
+            height: deviceHeight * 0.02,
+          ),
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Completed extends StatelessWidget {
+  const Completed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double unitHeightValue = MediaQuery.of(context).size.height * 0.01;
+    final deviceWidth = getDeviceWidth(context);
+    final deviceHeight = getDeviceHeight(context);
+    return Container(
+      // height: getDeviceHeight(context) * .4,
+      child: ListView(
+        children: [
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+          SizedBox(
+            height: deviceHeight * 0.02,
+          ),
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+          SizedBox(
+            height: deviceHeight * 0.02,
+          ),
+          EventFeed(
+            deviceWidth: deviceWidth,
+            deviceHeight: deviceHeight,
+            unitHeightValue: unitHeightValue,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventFeed extends StatelessWidget {
+  const EventFeed({
+    Key? key,
+    required this.deviceWidth,
+    required this.deviceHeight,
+    required this.unitHeightValue,
+  }) : super(key: key);
+
+  final deviceWidth;
+  final deviceHeight;
+  final double unitHeightValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(deviceWidth * 0.03),
+      constraints: BoxConstraints(maxHeight: deviceHeight * 0.22),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          // picture
+          Container(
+            margin: EdgeInsets.only(right: deviceWidth * 0.05),
+            width: deviceWidth * 0.38,
+            height: deviceHeight * 0.2,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/blockchain.png"),
               ),
             ),
-            // second box
-            Column(
-              // mainAxisSize: MainAxisSize.max,
+          ),
+          // second box
+          SizedBox(
+            width: deviceWidth * 0.38,
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Text(
                   "Blockchain",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 3 * unitHeightValue),
                 ),
                 SizedBox(
                   height: deviceHeight * 0.015,
@@ -98,44 +221,51 @@ class UserPage extends StatelessWidget {
                   'Start date: 04-09-22',
                   overflow: TextOverflow.ellipsis,
                   style: TextStyle(
-                    fontSize: 12,
+                    fontSize: 1.5 * unitHeightValue,
                   ),
                 ),
                 SizedBox(
                   height: deviceHeight * 0.01,
                 ),
                 Text(
-                  'Event: Online',
-                  overflow: TextOverflow.ellipsis,
+                  'Venue: Onlinefhdsbfhjdsfhdsbfsdjhfbdshjfbdhjfbdshjfbdshf',
                   style: TextStyle(
-                    fontSize: 12,
+                    fontSize: 1.5 * unitHeightValue,
                   ),
                 ),
                 SizedBox(
-                  height: deviceHeight * 0.035,
+                  height: deviceHeight * 0.03,
                 ),
                 Text(
                   "Duration: 3 Days",
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-              ],
-            ),
-            //third box
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.arrow_circle_right,
-                    color: Theme.of(context).primaryColor,
-                    size: 40,
+                  style: TextStyle(
+                    color: Colors.grey[600],
+                    fontSize: 1.5 * unitHeightValue,
                   ),
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          //third box
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EventDetail(),
+                  ),
+                ),
+                icon: Icon(
+                  Icons.arrow_circle_right,
+                  color: Theme.of(context).primaryColor,
+                  size: 40,
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
